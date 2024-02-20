8a7ddafe8a671474b67e45afa2ef47fa3b52cca7
patrikduch <duchpatrik@icloud.com>
2024-02-20 22:15:01 +0100

    Initial commit

---
+++ src/main/java/com/patrikduch/weatherspringbootbe/WeatherSpringBootBeApplication.java
@@ -0,0 +1,13 @@
+package com.patrikduch.weatherspringbootbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSpringBootBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherSpringBootBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/patrikduch/weatherspringbootbe/WeatherSpringBootBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrikduch.weatherspringbootbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSpringBootBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
